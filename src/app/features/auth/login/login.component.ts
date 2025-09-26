e025cb2ba3d4a3680c075d7315a7a3446aeef743
AxelSC <axelsc.dev@gmail.com>
2025-09-26 23:30:51 +0200

    Refactor CSS and components done

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 // Angular Material
@@ -12,7 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDivider } from '@angular/material/divider';
 
-//átomos standalone
+// Átomo: fondo animado
 import { SpaceBackgroundComponent } from '../../../layout/space-background/space-background.component';
 
 @Component({
@@ -27,12 +26,46 @@ import { SpaceBackgroundComponent } from '../../../layout/space-background/space
     MatButtonModule,
     MatIconModule,
     MatCheckboxModule,
-    SpaceBackgroundComponent,
     MatDivider,
+    SpaceBackgroundComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  // La lógica la añadirás después
+  @Input() brandIcon: string = 'shield';
+  @Input() brandName: string = 'Lumen Dashboard';
+  @Input() welcomeMessage: string = 'Welcome back!';
+  @Input() buttonLabel = 'Login';
+  @Input() loading = false; //For loading purposes
+  @Input() showRegisterLink = true; //For private use
+
+  @Output() login = new EventEmitter<{ email: string; password: string }>();
+  @Output() register = new EventEmitter<void>();
+
+  showPassword = signal(false);
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+  });
+
+  onSubmit() {
+    if (this.form.valid && !this.loading) {
+      this.login.emit(this.form.getRawValue());
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  onRegister() {
+    this.register.emit();
+  }
+
+  togglePassword() {
+    this.showPassword.update((v) => !v);
+  }
 }
